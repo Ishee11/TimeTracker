cf83fa4c4580ae1d4a168bba1629ab45b94856b6
Ishee11 <112874269+Ishee11@users.noreply.github.com>
2024-09-30 09:56:21 +1000

    Update database.go
    Удаление миграции из кода

--- statActivity/database/database.go
+++ statActivity/database/database.go
@@ -42,34 +42,5 @@ func ConnectDB() (*pgxpool.Pool, error) {
 
 	fmt.Println("Подключение к базе данных PostgreSQL успешно выполнено")
 
-	// Выполняем миграцию модели Activity
-	err = migrateActivities(connPool)
-	if err != nil {
-		log.Fatal("Ошибка миграции модели:", err)
-		return nil, err
-	}
-
-	return connPool, nil
-}
-
-// migrateActivities создает таблицу для модели Activity, если она не существует
-func migrateActivities(connPool *pgxpool.Pool) error {
-	ctx := context.Background()
-
-	// SQL-запрос для создания таблицы
-	createTableSQL := `
-	CREATE TABLE IF NOT EXISTS activities (
-		id SERIAL PRIMARY KEY,
-		type VARCHAR(255) NOT NULL,
-		time VARCHAR(50) NOT NULL,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-	);`
-
-	// Выполняем запрос
-	_, err := connPool.Exec(ctx, createTableSQL)
-	if err != nil {
-		return fmt.Errorf("не удалось создать таблицу activities: %v", err)
-	}
-
 	return nil
 }
